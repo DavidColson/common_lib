57b092ad4d1ab42e39ed56331da080be8335da0f
David Colson <davidcncolson@gmail.com>
2022-09-26 22:13:42 +0100

    Notes about allocator design

--- tests_main.cpp
+++ tests_main.cpp
@@ -25,13 +25,21 @@ int arrayTest() {
 // TODO: 
 // 1. Make a dynamic array class
 // 2. Make a custom allocator mechanism for the dynamic array class
-// 2a. Toy with global context allocator objects 
-// 2b. Toy with global allocator stack to push/pop allocators for all to access
 // 3. Make a dynamic string class inspired by bx stringT and built on the dynamic array 
 // 4. Make a string view class
 // 5. Make a simple hash map class
 // 6. Make a memory tracker using custom allocator
 // 7. Make other allocators (such as jai's pool alloc)
+
+
+// Re: Allocators (point 2)
+//
+// I have the feeling that a virtual function allocator interface is actually going to be fine
+// So we can avoid the template allocator parameter. And just have set/get allocator functions on containers
+// Things that will free much later than they alloc must store the allocator for later freeing
+// Everything else that does mem alloc should take an allocator as a parameter to make it clear they're doing mem ops
+// I think we should have a global mem allocator object that anyone can access
+// Regarding global context stack, we can probably play with this as a bonus if needed, but lets see how far we get without it first please
 
 int main() {
     printf("hello world");
