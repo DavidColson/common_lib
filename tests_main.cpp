0c0e7a934e862c47c06b09a731d3e6869c45f4b6
David Colson <davidcncolson@gmail.com>
2022-10-03 22:30:28 +0100

    Completed implementation of array class with fancier functions, and test for all added functionality. Also added a debug check macro for bounds checking in debug builds.

--- tests_main.cpp
+++ tests_main.cpp
@@ -7,6 +7,11 @@
 // Core Types
 typedef unsigned int uint32;
 
+#ifdef _DEBUG
+#define DEBUG_CHECK(expression) if (!(expression)) __debugbreak();
+#else
+#define DEBUG_CHECK(expression)
+#endif
 
 template<typename type>
 struct Array {
@@ -25,7 +30,7 @@ struct Array {
 		capacity = desiredCapacity;
 	}
 
-	void append(const type& value) {
+	void push_back(const type& value) {
 		if (count == capacity) {
 			reserve(grow_capacity());
 		}
@@ -33,17 +38,92 @@ struct Array {
 		count++;
 	}
 
-	// TODO: Erase, Insert, iterator type
+	void pop_back() {
+		DEBUG_CHECK(count > 0);
+		count--;
+	}
 
-	type& operator[](int i) {
+	type& operator[](uint32 i) {
+		DEBUG_CHECK(i >= 0 && i < count);
 		return pData[i];
 	}
 	
-	const type& operator[](int i) const {
+	const type& operator[](uint32 i) const {
+		DEBUG_CHECK(i >= 0 && i < count);
 		return pData[i];
 	}
 
-	bool validate() {
+	void clear() {
+		free(pData);
+		pData = nullptr;
+		count = 0;
+		capacity = 0;
+	}
+
+	void erase(uint32 index) {
+		DEBUG_CHECK(index >= 0 && index < count);
+		if (index == count-1) {
+			pop_back(); count--;
+		}
+		if (index < count-1) {
+			memmove(pData + index, pData + (index + 1), (count - index) * sizeof(type));
+			count--;
+		}
+	}
+
+	void erase_unsorted(uint32 index) {
+		DEBUG_CHECK(index >= 0 && index < count);
+		if (index == count-1) {
+			pop_back(); count--;
+		}
+		if (index < count-1) {
+			memcpy(pData + index, pData + (count - 1), sizeof(type));
+			count--;
+		}
+	}
+	
+	void insert(uint32 index, const type& value) {
+		DEBUG_CHECK(index >= 0 && index < count);
+		if (capacity == count) reserve(grow_capacity());
+		memmove(pData + (index + 1), pData + index, (count-index) * sizeof(type));
+		memcpy(pData + index, &value, sizeof(type));
+		count++;
+	}
+
+	type* find(const type& value) {
+		type* pTest = pData;
+		const type* pDataEnd = pData + count;
+		while (pTest < pDataEnd) {
+			if (*pTest == value)
+				break;
+			pTest++;
+		}
+		return pTest;
+	}
+
+	type* begin() {
+		return pData;
+	}
+
+	type* end() {
+		return pData + count;
+	}
+
+	const type* begin() const{
+		return pData;
+	}
+
+	const type* end() const {
+		return pData + count;
+	}
+
+	uint32 index_from_ptr(const type* ptr) const {
+		DEBUG_CHECK(ptr >= pData && ptr < pData + count);
+		ptrdiff_t diff = ptr - pData;
+		return (uint32)diff;
+	}
+
+	bool validate() const {
 		return capacity >= count;
 	}
 
@@ -53,7 +133,7 @@ private:
 		if (capacity == 0)
 			newCapacity = 8;
 		else
-			newCapacity = capacity + capacity / 2;
+			newCapacity = 2 * capacity;
 		return newCapacity;
 	}
 };
@@ -89,35 +169,85 @@ bool _verify(bool expression, int& errorCount, const char* file, int line, const
 #define VERIFY(expression) _verify((expression), errorCount, __FILE__, __LINE__, (#expression))
 #define VERIFY_MSG(expression, msg) _verify((expression), errorCount, __FILE__, __LINE__, msg)
 
+
+
+
+
+// ---------------------
+// Tests
+// ---------------------
+
 int arrayTest() {
 	StartTest("Array Test");
 	int errorCount = 0;
 
 	Array<int> testArray;
 
+	// Reserve memory
 	testArray.reserve(2);
-	// TODO: Could verify the amount of memory requested from allocator?
 	VERIFY(testArray.capacity == 2);
 	
-	testArray.append(1337);
-	testArray.append(420);
-	testArray.append(1800);
-	testArray.append(77);
-	testArray.append(99);
+	// Appending values
+	testArray.push_back(1337);
+	testArray.push_back(420);
+	testArray.push_back(1800);
+	testArray.push_back(77);
+	testArray.push_back(99);
 	VERIFY(testArray[0] == 1337);
 	VERIFY(testArray[1] == 420);
 	VERIFY(testArray[2] == 1800);
 	VERIFY(testArray[3] == 77);
 	VERIFY(testArray[4] == 99);
 	VERIFY(testArray.count == 5);
-	VERIFY(testArray.capacity == 6);
+	VERIFY(testArray.capacity == 8);
 
-	// Reserve should memcpy the old data to the new reserved location
+	// Reserve should memcpy the old data to the new reserved location 
 	testArray.reserve(50);
 	VERIFY(testArray[2] == 1800);
 	VERIFY(testArray.capacity == 50);
 	VERIFY(testArray.count == 5);
 	
+	// Iteration
+	int sum = 0;
+	for (int i : testArray) {
+		sum += i;
+	}
+	VERIFY(sum == 3733);
+
+	// Erasing elements
+	testArray.erase(2);
+	testArray.erase_unsorted(0);
+	VERIFY(testArray[0] == 99);
+	VERIFY(testArray[1] == 420);
+	VERIFY(testArray[2] == 77);
+	VERIFY(testArray.count == 3);
+
+	// Remove element at back
+	testArray.pop_back();
+	VERIFY(testArray.count == 2);
+
+	// Inserting elements
+	testArray.insert(0, 9999);
+	testArray.insert(1, 1111);
+	testArray.insert(1, 2222);
+	VERIFY(testArray[0] == 9999);
+	VERIFY(testArray[1] == 2222);
+	VERIFY(testArray[2] == 1111);
+	VERIFY(testArray[3] == 99);
+	VERIFY(testArray[4] == 420);
+	VERIFY(testArray.count == 5);
+
+	// Finding elements
+	VERIFY(testArray.index_from_ptr(testArray.find(99)) == 3);
+	VERIFY(testArray.index_from_ptr(testArray.find(1111)) == 2);
+	VERIFY(testArray.find(1337) == testArray.end());
+
+	// Clear the array
+	testArray.clear();
+	VERIFY(testArray.count == 0);
+	VERIFY(testArray.capacity == 0);
+	VERIFY(testArray.pData == nullptr);
+
 	// You wanna test for mem leaks at the end of this function
 	// Plus test for memory overwrites and so on (tests should maybe be done in address sanitizer mode)
 
@@ -126,13 +256,18 @@ int arrayTest() {
 }  
 
 // TODO: 
-// 1. Make a dynamic array class
-// 2. Make a custom allocator mechanism for the dynamic array class
-// 3. Make a dynamic string class inspired by bx stringT and built on the dynamic array 
-// 4. Make a string view class
+// 1. Make a dynamic array class ------ DONE
+// 2. Install VS2022 and try out address sanitizer
+// 3. Make a string view class
+// 4. Make a dynamic string class inspired by bx stringT and built on the dynamic array 
 // 5. Make a simple hash map class
-// 6. Make a memory tracker using custom allocator
-// 7. Make other allocators (such as jai's pool alloc)
+// 6. Override global allocation functions to do memory tracking for debugging (new, delete, alloc, free)
+// 7. Make a custom allocator mechanism for the dynamic array class
+// 8. Make linear pool allocator
+
+
+// Jai's Pool allocator
+// Linked list of blocks basically, allocates a new one if you try get memory more than is free in the existing block
 
 
 // Re: Allocators (point 2)
@@ -145,11 +280,15 @@ int arrayTest() {
 // Regarding global context stack, we can probably play with this as a bonus if needed, but lets see how far we get without it first please
 
 
-// Re: Tests
-// https://github.com/maghoff/jsonxx/tree/master/tests
+// Re: Memory debug tracking
+//
+// Memory debugger.jai is a good example here.
+// on alloc we add the allocation to a table
+// on free, we look for the allocation in the table
+
 
 int main() {
 	arrayTest();
-    printf("hello world");
+	__debugbreak();
     return 0;
 }
